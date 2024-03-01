178a01977249dd9ab6e04438c769f730ac9ec38a
Jacob kaufenberg <Jacob.kaufenberg@icloud.com>
2024-03-01 16:19:15 -0600

    added the category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { update } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
@@ -17,24 +18,69 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id,{
-      include: [{}]
+      include: [{ model: Product}]
     
     })
+    if(!categoryData){
+      res.status(404).json({ message: 'no location found'})
+      return;
+    }
+
+    res.status(200).json(categoryData)
+  } catch (err){
+    res.status(500).json(err);
   }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const categoryData = await Category.create(req.body)
+    res.status(200).json(categoryData);
+  } catch (err){
+    res.status(400).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (updatedCategory[0] === 0) {
+      res.status(404).json({ message: 'Category not found' });
+    } else {
+      res.status(200).json(updatedCategory);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
   // update a category by its `id` value
-});
 
-router.delete('/:id', (req, res) => {
+
+router.delete('/:id',  async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!categoryData){
+      res.status(404).json({ message: 'no location of id'});
+      return;
+    }
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
